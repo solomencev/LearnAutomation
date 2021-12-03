92c741b041f7c40f7589b499b6e4b9779af2f212
iurii solomentsev <emirateskos@gmail.com>
2021-12-03 20:08:02 +0300

    added TestOpenYouTubeInIE

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,2 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
 public class SearchTest {
+
+    @Test
+    public void TestOpenYouTubeInIE(){
+        File file = new File("src/test/resources/");
+        System.setProperty("webdriver.chrome.driver", "D:/LearnAutomation/src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://www.youtube.com/");
+        driver.navigate().refresh();
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
 }
