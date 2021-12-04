278148b882ae1b4edfcc8ecb9bf38ff317db7203
iurii solomentsev <emirateskos@gmail.com>
2021-12-04 08:22:38 +0300

    added getText and getAttribute

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,4 +1,7 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -7,13 +10,18 @@ import java.io.File;
 public class SearchTest {
 
     @Test
-    public void TestOpenYouTubeInIE(){
-        File file = new File("src/test/resources/");
-        System.setProperty("webdriver.chrome.driver", "D:/LearnAutomation/src/test/resources/chromedriver.exe");
+    public void TestOpenGoogleInChrome(){
+        File file = new File("src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
         WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://www.youtube.com/");
-        driver.navigate().refresh();
-        System.out.println(driver.getTitle());
+        driver.get("https://www.google.com/");
+        WebElement searchField = driver.findElement(By.cssSelector("input[name='q']"));
+        searchField.clear();
+        searchField.sendKeys("selenium java");
+        searchField.sendKeys(Keys.RETURN);
+        WebElement resultRow = driver.findElement(By.xpath("//div[@class='g']//h3"));
+        System.out.println(resultRow.getText());
+        System.out.println(resultRow.getAttribute("class"));
         driver.quit();
     }
 }
