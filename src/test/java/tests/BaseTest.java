a742a4d09a9a19f579c53133630bcc3b5788abbf
iurii solomentsev <emirateskos@gmail.com>
2021-12-05 15:54:59 +0300

    add framework.properties

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.DataProvider;
 import steps.SearchSteps;
 import utils.Browser;
 import utils.DriverFactory;
+import utils.PropertyReader;
 
 import java.io.File;
 
@@ -22,8 +23,8 @@ public abstract class BaseTest {
 
     @BeforeClass
     public void SetUp() {
-        driver = DriverFactory.getDriver(Browser.CHROME);
-        driver.get("https://www.google.com/");
+        driver = DriverFactory.getDriver(PropertyReader.getBrowser());
+        driver.get(PropertyReader.getUrl());
         steps = new SearchSteps();
     }
 
